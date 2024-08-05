276d564eebd0195b5541576b3722f6938207ccd7
dmQuang253 <quangdao2004qq@gmail.com>
2024-08-06 00:29:07 +0700

    ngay1

---
+++ src/main/java/com/example/on_tap_j6_b1/OnTapJ6B1Application.java
@@ -0,0 +1,13 @@
+package com.example.on_tap_j6_b1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnTapJ6B1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnTapJ6B1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/on_tap_j6_b1/OnTapJ6B1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.on_tap_j6_b1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTapJ6B1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
